531fbb13f48f81d7284067192ccbca3cc96615d3
Lemonnik6484 <LemonGamer6484@gmail.com>
2024-06-22 12:29:03 +0200

    Planned Map
    Updated converter (more element support)
    Updated current map dark variant
    Added planned map
    Url changed

--- color_conventer/converter.py
+++ color_conventer/converter.py
@@ -3,14 +3,11 @@ import pathlib
 import os
 
 def change_svg_path_color(input_svg, output_svg, old_color, new_color):
-    # Парсинг SVG файла
     tree = ET.parse(input_svg)
     root = tree.getroot()
     
-    # Определяем пространство имен SVG
     ns = {'svg': 'http://www.w3.org/2000/svg'}
     
-    # Проходим через все элементы path и меняем цвет
     for path in root.findall('.//svg:path', ns):
         style = path.get('style')
         if style and f'fill:{old_color}' in style:
@@ -19,16 +16,13 @@ def change_svg_path_color(input_svg, output_svg, old_color, new_color):
         elif path.get('fill') == old_color:
             path.set('fill', new_color)
     
-    # Сохраняем измененный SVG в новый файл
     tree.write(output_svg)
 
 def change_stroke_color(svg_file, original_color, new_color, new_svg_file):
     tree = ET.parse(svg_file)
     root = tree.getroot()
     
-    # Find all rectangles in the SVG file
     for rect in root.iter('{http://www.w3.org/2000/svg}rect'):
-        # Check if the rectangle has the specified stroke color
         if rect.get('stroke') == original_color:
             rect.set('stroke', new_color)
     
@@ -38,21 +32,47 @@ def change_rect_fill_color(svg_file, original_color, new_color, new_svg_file):
     tree = ET.parse(svg_file)
     root = tree.getroot()
     
-    # Find all rectangles in the SVG file
     for rect in root.iter('{http://www.w3.org/2000/svg}rect'):
-        # Check if the rectangle has the specified stroke color
         if rect.get('fill') == original_color:
             rect.set('fill', new_color)
     
     tree.write(new_svg_file)
 
-# Использование функции
-input_one = './map.svg'
+def change_path_stroke_color(input_file, old_color, new_color, output_file):
+    tree = ET.parse(input_file)
+    root = tree.getroot()
+    
+    ns = {'svg': 'http://www.w3.org/2000/svg'}
+    
+    for path in root.findall('.//svg:path', ns):
+        style = path.get('style')
+        if style and old_color in style:
+            new_style = style.replace(old_color, new_color)
+            path.set('style', new_style)
+        
+        fill = path.get('fill')
+        if fill == old_color:
+            path.set('fill', new_color)
+        
+        stroke = path.get('stroke')
+        if stroke == old_color:
+            path.set('stroke', new_color)
+
+    tree.write(output_file)
+
+input_one = './current/map.svg'
 old_color = '#FCFAF3'
 new_color = '#000000'
 result_one = f'{pathlib.Path(__file__).parent.resolve()}\\output.svg'
 
 change_stroke_color(input_one, old_color, new_color, result_one)
+
+input_one = f'{pathlib.Path(__file__).parent.resolve()}\\output.svg'
+old_color = '#FCFAF3'
+new_color = '#080E1E'
+result_one = f'{pathlib.Path(__file__).parent.resolve()}\\output.svg'
+
+change_path_stroke_color(input_one, old_color, new_color, result_one)
 
 input_one = f'{pathlib.Path(__file__).parent.resolve()}\\output.svg'
 old_color = 'white'
@@ -83,9 +103,16 @@ result_one = f'{pathlib.Path(__file__).parent.resolve()}\\output.svg'
 change_rect_fill_color(input_one, old_color, new_color, result_one)
 
 input_one = f'{pathlib.Path(__file__).parent.resolve()}\\output.svg'
+old_color = '#FCFAF3'
+new_color = '#080E1E'
+result_one = f'{pathlib.Path(__file__).parent.resolve()}\\output.svg'
+
+change_rect_fill_color(input_one, old_color, new_color, result_one)
+
+input_one = f'{pathlib.Path(__file__).parent.resolve()}\\output.svg'
 old_color = '#E0DDD0'
 new_color = '#55648E'
-result_one = './dark_map.svg'
+result_one = './current/dark_map.svg'
 
 change_stroke_color(input_one, old_color, new_color, result_one)
 
